c02cc2e015f3f04a4968020884dc21ce6d09558b
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-10-20 09:34:13 +0200

    main menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class OptionsMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame()
+    void Start()
     {
-        //SceneManager.LoadScene("Level_0")
+        
     }
 
     // Update is called once per frame
-    public void QuitGame()
+    void Update()
     {
-       Application.Quit(); 
+        
     }
 }
