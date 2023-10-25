b8285dbb2a77b283aca924a9c3fbbb0ce5ee30d6
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-10-25 12:28:20 +0200

    play commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     public void PlayGame()
     {
-        //SceneManager.LoadScene("Level_0")
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
