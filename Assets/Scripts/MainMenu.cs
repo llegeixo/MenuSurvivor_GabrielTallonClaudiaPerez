f40d101ead346627093dadd5efad6c00dee2aa02
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-11-15 13:05:11 +0100

    mainmenu finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject _book;
+    [SerializeField] RectTransform _bookTransform;
+    [SerializeField] float _initialPosition;
+    [SerializeField] float _finalPosition; 
+    [SerializeField] float _bookSpeed = 5f;
     
     
     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -32,5 +42,35 @@ public class MainMenu : MonoBehaviour
     public void GoToGameMenu()
     {
         SceneManager.LoadScene("GameMenu");
+    }
+
+    public void OpenOptions()
+    {
+        StartCoroutine("Book");
+    }
+
+    public void OpenMenu()
+    {
+        StartCoroutine("ReturnMenu");
+    }
+
+    IEnumerator Book()
+    {
+        
+        while (_bookTransform.position.y > _finalPosition)
+        {
+            _bookTransform.localPosition -= new Vector3(0, 1 * _bookSpeed, 0);
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+
+    IEnumerator ReturnMenu()
+    {
+        while (_bookTransform.position.y <_initialPosition)
+        {
+            _bookTransform.localPosition += new Vector3(0, 1 * _bookSpeed, 0);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
